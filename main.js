f8e6c732d7317b4e60d47b43a263acacd35e9cec
Cristian Pineda <cristiap17@gmail.com>
2023-01-19 21:36:27 -0500

    Haciendo arreglos y dejando el componente optimo

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 const cardContainer = document.getElementById('cardContainer')
 const numberSelected = document.getElementById('numberSelected')
-
 const submitContent = document.getElementById('submit-content')
 const submitButton = document.getElementById('submitButton')
 
-const radioContainer = document.getElementsByClassName('ratio-content')[0].children
-
+const radioContainer = document.getElementById('hola').children
+console.log(radioContainer);
 
 submitButton.addEventListener('click', toogleSubmitState)
 
@@ -21,11 +20,50 @@ function toogleSubmitState() {
 
 function backGraundChange(id) {
     const ratioCheck = document.getElementById(id)
-    console.log(ratioCheck.innerText);
-        ratioCheck.style.backgroundColor = '#838c95'
-        ratioCheck.style.color = 'white' 
+    
+    changeSelect(ratioCheck.id)
+    numberSecc(ratioCheck.innerText)
+}
 
-        numberSecc(ratioCheck.innerText)
+
+let prev;
+let current;
+
+function changeSelect(ratioSelect) {
+
+    if (!current) {
+        current = ratioSelect;
+    } else {
+        prev = current;
+        current = ratioSelect
+    }
+
+    const kbdCurrent = document.getElementById(current)
+    kbdCurrent.style.backgroundColor = '#838c95'
+    kbdCurrent.style.color = 'white'
+
+
+    if (prev){
+        const kbdPrev = document.getElementById(prev)
+        kbdPrev.style.backgroundColor = '#272e38'
+        kbdPrev.style.color = '#666e79'
+    } 
+
+    console.log(prev, current);
+
+    //OTRA MANERA DE EJECUTAR EL CICLO PARA EL CAMBIO DE SELECCION
+
+    /* for (let index = 0; index < radioContainer.length; index++) {
+        if (radioContainer[index].id === ratioSelect){
+            radioContainer[index].style.backgroundColor = '#838c95'
+            radioContainer[index].style.color = 'white'
+        } else {
+            radioContainer[index].style.backgroundColor = '#272e38'
+            radioContainer[index].style.color = '#666e79'
+        }
+        
+    } */
+
 }
 
 function numberSecc(numberSelec) {
